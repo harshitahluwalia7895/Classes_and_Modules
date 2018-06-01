f56f3c9855e91701f135dcdd506b441255386dbc
harshitahluwalia7895 <harshit.ahluwalia7895889924@gmail.com>
2018-06-01 11:38:38 +0530

    Final Commit

---
+++ answer1.py
@@ -0,0 +1,15 @@
+import math as m
+class Circle:
+
+        def __init__(self,r):
+            self.radius=r
+
+        def getArea(self):
+            return m.pi*((self.radius)**2)
+
+        def getCircumference(self):
+            return 2*(m.pi)*self.radius
+
+r=float(input("Enter radius of circle: "))
+c1=Circle(r)
+print("Radius={} Area={} Circumference={}".format(c1.radius,c1.getArea(),c1.getCircumference()))
